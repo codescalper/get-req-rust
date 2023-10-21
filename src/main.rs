8fa71ae1923f11bdedc6cb1c8c1c661c0045d571
codescalper <msfunbook@gmail.com>
2023-10-21 11:02:32 +0530

    simple get equest using `reqwest` crate

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
-fn main() {
-    println!("Hello, world!");
+use error_chain::error_chain;
+use std::io::Read;
+
+error_chain! {
+    foreign_links{
+        Io(std::io::Error);
+        HttpRequest(reqwest::Error);
+    }
+}
+
+fn main() -> Result<()> {
+    let mut res = reqwest::blocking::get("https://dummyjson.com/products/1")?;
+    let mut body = String::new();
+    res.read_to_string(&mut body)?;
+
+    println!("Status: {:?}", res.status());
+    println!("Body: \n{:#?}", body);
+    println!("Status: {:?}", res.headers());
+    Ok(())
 }
